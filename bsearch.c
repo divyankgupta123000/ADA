01f83df85ce096ad67eb225947b965d1c53211bd
divyankgupta123000 <53806810+divyankgupta123000@users.noreply.github.com>
2020-02-06 22:26:21 +0530

    Update bsearch.c

--- bsearch.c
+++ bsearch.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
-int b_search(int k,int a[50],int n);
+void b_search(int k,int a[50],int n);
 
 void main()
 {
 	int a[50];
-	int i,k,n,res;
+	int i,k,n;
 	printf("Enter total number of values: ");
 	scanf("%d",&n);
 	printf("Enter sorted array values:\n");
@@ -13,75 +13,80 @@ void main()
 	  scanf("%d",&a[i]);
 	printf("\nEnter Key value to be searched: ");
 	scanf("%d",&k);
-	res=b_search(k,a,n);
-	if(res==-1)
-	  printf("-1");
+	b_search(k,a,n);
 }
 
-int b_search(int k,int a[50],int n)
+void b_search(int k,int a[50],int n)
 {
-	int i=0,res=0;
+	int i=0,res=-1;
 	int b[50],count=0,min,max;
-	int first=0,mid,last=n-1;
-	mid=(first+last)/2;
-	
-	if(res==-1)
-	 return res;
-	 
-	while(first<=last || res==-1)
+	int first,mid,last;
+
+	do
 	{
-		
+	  first=0;
+	  last=n-1;
+	  mid=(first+last)/2;
+	  while(first<=last)
+	  {
 	    if(k<a[mid])
 	    {
-		    last=a[mid]-1;
+		    last=mid-1;
 		    mid=(first+last)/2;
 	    }
 	    else if(k>a[mid])
 	    {
-		    first=a[mid]+1;
+		    first=mid+1;
 		    mid=(first+last)/2;
 	    }
 		else if(k==a[mid])
 		{
 			b[i]=mid;
-			i++;
-			b[i]=-2;
+			b[++i]=-2;
 		    a[mid]=-99;
-		} 
-	   else
-	    res=-1;
-	}
+		    res=0;
+		    break;
+		}
+		else
+        {
+            if(first=last && a[mid]!=k)
+             break;
+        }
+	  }
+	}while(first<=last);
+
 	i=0;
-	if(res!=-1)
-	{
-		printf("Total Count= %d",1);
-		printf("First Occurence: %d\n",mid);
-		printf("Last Occurence: %d\n",mid);
-	}
-   while(b[i]!=-2)
-	{
+	if(res==-1)
+    {
+      printf("-1");
+    }
+    else
+    {
+      while(b[i]!=-2)
+	  {
 		count++;
 	    i++;
-	}
-	
-	printf("Total Count= %d",count);
-	
-	min=b[0];
-	max=b[0];
-	for(i=1;i<count;i++)
-	{
+	  }
+      for(i=0;i<count;i++)
+        printf("%d",b[i]);
+	  printf("\nTotal Count= %d\n",count);
+
+	  min=b[0];
+	  max=b[0];
+	  for(i=1;i<count;i++)
+	  {
 		if(b[i]<min)
 			min=b[i];
-	}
-	
-    printf("First Occurence: %d\n",min);
-	
-	for(i=1;i<count;i++)
-	{
-		if(b[i]<min)
-			min=b[i];
-	}
-	
-	printf("Last Occurence: %d\n",max);
-	return 0;
+      }
+
+      printf("First Occurence: %d\n",min+1);
+
+	  for(i=1;i<count;i++)
+	  {
+		if(b[i]>max)
+			max=b[i];
+	  }
+
+	  printf("Last Occurence: %d\n",max+1);
+   }
 }
