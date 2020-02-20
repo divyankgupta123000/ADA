63ca2354816295f05d56b4e2c1faf08792dd4d49
divyankgupta123000 <53806810+divyankgupta123000@users.noreply.github.com>
2020-02-20 18:30:52 +0530

    Update Smatch.java

--- Smatch.java
+++ Smatch.java
@@ -12,31 +12,34 @@ class Smatch
      SmatchDemo sd=new SmatchDemo();
      int res=sd.stest(s1,s2);
      if (res!=-1)
-       System.out.println("Pattern found at" + res+"th position");
+       System.out.println("Pattern found at position "+res);
      else
-       System.out.println("Patter not found!!");
+       System.out.println("Pattern not found!!");
   }
 }
 
 class SmatchDemo
 {
+   int i,j;
+   
    int stest(String s1,String s2)
    {
-      for(int i=0;i<s1.length()-s2.length();i++)
+      if(s1.length()>=s2.length())
       {
-        for(int j=0;j<s2.length();j++)
-        {
-           if(s1.charAt(i+j)==s2.charAt(j))
-             j++;
-           else
-             break;
-        }
-        if(int j==s2.length())
-          return i;
-     }
-     return -1;
-  }
-
-}
-
+         for(i=0;i<=s1.length()-s2.length();i++)
+         {
+            for(j=0;j<s2.length();)
+            {
+               if(s1.charAt(i+j)==s2.charAt(j))
+                 j++;
+               else
+                 break;
+            }
+           if(j==s2.length())
+             return (i+1);
+         }
+      }
+      return -1;
+    } 
+ }
 
