181f76e797b98138d20913882e78fda239cdd345
divyankgupta123000 <53806810+divyankgupta123000@users.noreply.github.com>
2020-04-26 20:42:06 +0530

    Update DFS.java

--- DFS.java
+++ DFS.java
@@ -1,134 +1,70 @@
 import java.util.*;
 
-class DFS 
+class DFS
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter total no. of vertices: ");
+        System.out.println("Enter Total Vertices:-");
         int v=sc.nextInt();
-        int a[][]=new int[v][v];
-        System.out.println("Enter adjacency matrix: ");
+        System.out.println("Enter Adjacency Matrix:-");
+        int arr[][]=new int[v][v];
         for(int i=0;i<v;i++)
-        {  
+        {
             for(int j=0;j<v;j++)
-                a[i][j]=sc.nextInt();
+            {
+                arr[i][j]=sc.nextInt();
+            }
         }
-        Traversal t=new Traversal(v);
-        t.dfs(a,0);
-        if(t.connect==1)
-            System.out.println("\nGraph is connected !!!");
+        System.out.println("Enter Starting Vertex:-");
+        int start=sc.nextInt();
+        int state[]=new int[v];
+        for(int i=0;i<v;i++)
+            state[i]=0;
+        DDemo d= new DDemo(v);
+        int connect=0;
+        System.out.print("Traversal Order:- ");
+        d.dfs(arr,state,start-1,-1);
+        int i=start%v;
+        for(;i!=start-1;i=(i+1)%v)
+        {
+            if(state[i]==0)
+            {
+                connect=-1;
+                d.dfs(arr,state,i,-1);
+            }
+        }
+        if(connect==0)
+            System.out.println("\nGraph is connected!!!");
         else
-            System.out.println("\nGraph is disconnected !!!");
-        if(t.c==1)
-            System.out.println("Graph is cyclic !!!");
+            System.out.println("\nGraph is disconnected!!!");
+        if(d.cycle==0)
+            System.out.println("Graph is cyclic!!!");
         else
-            System.out.println("Graph is acyclic !!!");
+            System.out.println("Graph is acyclic!!!");
     }
 }
 
-class Traversal
+class DDemo
 {
-    int stk[],state[],top,p=-1,c=0;
-    int i,j,v,connect=1,res,val=0;
-    
-    Traversal(int n)
+    int n,cycle=-1;
+    DDemo(int v)
     {
-        v=n;
-        stk=new int[50];
-        state=new int[n];
-        state[0]=1;
-        for(i=1;i<n;i++)
-            state[i]=0;
-        top=-1; 
-        if(n!=0)
-           push(1);
+        n=v;
     }
-    
-    void push(int ele)
+    void dfs(int a[][],int state[],int vno,int parent)
     {
-        stk[++top]=ele;
-        disp();
-    }
-    
-    void pop()
-    {
-        top--;
-    }
-    
-    void disp()
-    {
-        if(top==-1)
-                System.out.println("Stack is Empty!!!");
-        else
+        System.out.print(vno+1+" ");
+        state[vno]=1;
+        for(int i=0;i<n;i++)
         {
-                System.out.print(stk[top]+" ");
-        }
-    }
-    
-    int checkstate()
-    {
-        for(int k=0;k<v;k++)
-        {
-            if(state[k]==0)
+            if(parent!=-1)
             {
-                state[k]=1;
-                return k;
+                if(a[vno][i]==1 && state[i]==1 && i!=parent)
+                    cycle=0;
             }
+            if(a[vno][i]==1 && state[i]==0)
+                dfs(a,state,i,vno);
         }
-        return -1;
-    }
-    
-    void dfs(int a[][],int x)
-    {
-        i=x;
-        for(j=0;j<v;) 
-        {
-           if(a[i][j]==1 && state[j]==0)
-           {
-                state[j]=1;
-                p=i;
-                i=j;
-                push(j+1);
-                c=checkcycle(a);
-                j=-1;
-           }
-           j++;
-        }
-        pop();
-        if(top!=-1)
-            i=stk[top]-1;
-        if(i==val)
-        {
-            res=val;
-            val=checkstate();
-            if(val!=-1)
-            {
-                i=val;
-                if(a[res][val]==0)
-                {
-                    connect=0;
-                    if(top!=-1)
-                        pop();
-                }
-                push(val+1);
-            }
-            else
-                return;
-        }
-        dfs(a,i);
-    }
-    
-    int checkcycle(int a[][])
-    {
-        for(int k=0;k<i;k++)
-        {
-            if(a[i][k]==1 && k!=p)
-            { 
-                c=1;
-                return c;
-            }
-        }
-        return c;
     }
 }
